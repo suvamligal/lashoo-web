a63704016d6420c674683de0f937e356bad0a3e6
suvamligal <suvamligal@gmail.com>
2023-02-27 20:33:28 -0600

    initial commit

---
+++ src/main/java/lashooweb/LashooWebApplication.java
@@ -0,0 +1,13 @@
+package lashooweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LashooWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LashooWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lashooweb/LashooWebApplicationTests.java
@@ -0,0 +1,13 @@
+package lashooweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LashooWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
